cb16ccdf0e8a9193415fc42f2d6042be35cd1568
x4v1k <146961654+x4v1k@users.noreply.github.com>
2023-10-24 16:07:01 +0200

    Angular Changes V0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ViBook';
+  logoBlanco = [
+    {
+      src: './assets/img/ViBook(Blanco).png',
+      titulo: 'Logo ViBook Blanco',
+    }
+  ];
+  logoNegro = [
+    {
+      src: './assets/img/ViBook.png',
+      titulo: 'Logo ViBook Negro',
+    }
+  ];
+  logoLila = [
+    {
+      src: './assets/img/ViBook_lila.png',
+      titulo: 'Logo ViBook Lila',
+    }
+  ];
+  background = {
+    src: './assets/img/wallpapersden.com_abstract-neon-circle-shapes_3840x2160_animation (1).webm',
+
+  }
+  backgroundimg = {
+    src: './assets/img/123.png',
+  }
+  instagram = {
+    src: './assets/img/logoInstagram.png',
+    titulo: 'Logo Instagram',
+  }
+  twitter = {
+    src: './assets/img/logoTwitter.png',
+    titulo: 'Logo Twitter',
+  }
+  gmail = {
+    src: './assets/img/logoGmail.png',
+    titulo: 'Logo Gmail',
+  }
 }
